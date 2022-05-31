59d8250c13035bfffa6656dab9dfd565256a6636
PedroGont <pedrotolentino2002@gmail.com>
2022-05-30 19:23:28 -0700

    Problem2

--- Problem2.py
+++ Problem2.py
@@ -3,3 +3,19 @@
 # has not entered the word “exit”. Once the user enters “exit” 
 # the function will return and print the list of all distinct 
 # words starts with English alphabets. Like:
+
+def organizeList(list):
+    myList = sorted(list)
+    print(myList)
+    return myList
+
+def inputList():
+    word = str
+    words = []
+    while word != "exit":
+        word = str(input("word:"))
+        if word != "exit":
+            words.append(word)
+    organizeList(words)
+
+inputList()
