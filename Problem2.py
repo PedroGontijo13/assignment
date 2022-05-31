3f6d1033cf49809e1e5642fc128fad2247b32fe3
PedroGont <pedrotolentino2002@gmail.com>
2022-05-30 19:15:11 -0700

    problem6

---
+++ Problem2.py
@@ -0,0 +1,5 @@
+#Write a function which lets the user enter English words. 
+# The user can keep entering English words as long as the user
+# has not entered the word “exit”. Once the user enters “exit” 
+# the function will return and print the list of all distinct 
+# words starts with English alphabets. Like:
